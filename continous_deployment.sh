ddb5076ffddbfa173b5bab41653088a1c10572d4
Mostafa Gad <Gad.mostafa@gmail.com>
2020-05-05 17:54:46 +0200

    still travis

--- continous_deployment.sh
+++ continous_deployment.sh
@@ -3,7 +3,7 @@ set -ev
 export HEROKU_API_KEY=$HEROKU_AUTH
 
 echo $HEROKU_API_KEY
-# echo $HEROKU_AUTH | docker login --username=gad.mostafa@gmail.com --password-stdin registry.heroku.com
+echo $HEROKU_AUTH | docker login --username=gad.mostafa@gmail.com --password-stdin registry.heroku.com
 # docker login --username=mgad --password=$HEROKU_AUTH registry.heroku.com
 # heroku container:login
 heroku container:push --app=my-app-v4 web
