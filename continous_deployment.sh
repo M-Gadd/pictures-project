51dda4ca24f268c6c23e483335c00db6d3ce898b
Mostafa Gad <Gad.mostafa@gmail.com>
2020-05-05 15:34:03 +0200

    travis..

--- continous_deployment.sh
+++ continous_deployment.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # set -ev
-# echo $HEROKU_AUTH | docker login --username=mgad --password-stdin registry.heroku.com
-docker login --username=mgad --password=$HEROKU_AUTH registry.heroku.com
+echo $HEROKU_AUTH | docker login --username=mgad --password-stdin registry.heroku.com
+# docker login --username=mgad --password=$HEROKU_AUTH registry.heroku.com
 heroku container:push --app=my-app-v4 web
 heroku container:release --app=my-app-v4 web
 heroku open --app=my-app-v4
