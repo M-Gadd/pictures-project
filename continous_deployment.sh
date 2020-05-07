d6db6bac8cd7b12583932425565939902ca95458
Mostafa Gad <Gad.mostafa@gmail.com>
2020-05-07 19:39:04 +0200

    echoing travis branch

--- continous_deployment.sh
+++ continous_deployment.sh
@@ -1,13 +1,39 @@
 #! /bin/bash
 set -ev
+
 export HEROKU_API_KEY=$HEROKU_AUTH
 
-echo $HEROKU_API_KEY
-echo $HEROKU_AUTH | docker login --username=gad.mostafa@gmail.com --password-stdin registry.heroku.com
-# docker login --username=mgad --password=$HEROKU_AUTH registry.heroku.com
+echo $HEROKU_AUTH | docker login --username=_ --password-stdin registry.heroku.com
 # heroku container:login
-heroku container:push --app=my-app-v4 web
-heroku container:release --app=my-app-v4 web
+# heroku container:push --app=my-app-v4 web 
+# heroku container:release --app=my-app-v4 web
 # heroku open --app=my-app-v4
 # heroku logs --tail --app=my-app-v4
 
+
+
+#!/bin/bash
+set -ev
+
+if [ "${TRAVIS_BRANCH}" = "staging" ]; then
+  # kontena master login --token ${KONTENA_MASTER_TOKEN_STAGING} https://staging-master.bunch.ai/
+  # kontena grid use staging
+  # kontena stack install kontena.yml || kontena stack upgrade --force phoenix kontena.yml;
+fi
+
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+
+  echo "I AM IN MASTER"
+  # kontena master login --token ${KONTENA_MASTER_TOKEN_PRODUCTION} https://master.bunch.ai/
+  # kontena grid use production;
+  # kontena stack install kontena.yml || kontena stack upgrade --force phoenix kontena.yml;
+fi
+
+
+
+# heroku plugins:install @heroku-cli/plugin-container-registry
+# Dockerfile.client Dockerfile.server Dockerfile.nginx
+# heroku container:login
+# heroku container:push --recursive
+# heroku container:release client api nginx
+# heroku ps:scale client=1 api=1 nginx=1
